54dbc81a75cb20c760257c31bb63660d19c91016
Lera2323 <vassinalera@gmail.com>
2022-06-28 17:18:48 +0300

    task

---
+++ Program.cs
@@ -0,0 +1,108 @@
+﻿
+using System;
+
+namespace ConsolApp
+{
+    class Program
+    {
+        private static int index;
+
+        static void Main( string[]args)
+        {
+            /*
+
+            // Задача 2. Даны два числа. Показать большее и меньшее.
+
+            int a = 27;
+            int b = 3;
+        
+
+            if (a > b)
+
+            {
+                Console.WriteLine ("min: " + b);
+                Console.WriteLine ("max: " + a);
+            }
+            else
+            {
+                Console.WriteLine ("min: " + a);
+                Console.WriteLine ("max: " + b);
+                
+            }
+            */
+
+            /*
+
+            // Задача 4. Найти максимальное из 3х чисел.
+
+            int a = 5;
+            int b = 45;
+            int c = 38;
+            int max = a;
+
+            if (max < b)
+            {
+                max = b;
+            }
+            if (max < c)
+            {
+                max = c;
+            }
+
+            Console.WriteLine ("maximum: " + max);
+
+            */
+            /*
+
+
+            // Задача 4. Выяснить является ли число четным.
+
+            int a = 5;
+            
+            if (a%2 == 0)
+            {
+                Console.WriteLine (a + " is correct");
+            
+            }
+
+            else
+            {
+                Console.WriteLine (a + " is not correct");
+            }
+
+            */
+
+            // Показать четные числа от 1 до N.
+
+            int a = 13;
+            int b = 2;
+            
+            while (b < a)
+            {
+                Console.WriteLine (b);
+                b += 2;
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
+
+
+
